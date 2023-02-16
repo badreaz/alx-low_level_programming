693aead85168dae02c782b720f65be17e81587c8
badreaz <baz.23.2019@gmail.com>
2023-02-16 07:31:51 -0800

    create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print a quote
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar,";
+	char date[] = " 2015-10-19\n";
+
+	write(1, quote, sizeof(quote));
+	write(1, date, sizeof(date));
+	return (1);
+}
