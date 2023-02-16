c29648ca37508440c5e94b9f96f7899c9eb60fe6
badreaz <baz.23.2019@gmail.com>
2023-02-16 03:36:28 -0800

    create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a string using printf();
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the output is a piece of art,");
+	return (0);
+}
