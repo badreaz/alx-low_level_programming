0eb2c0d9471fbbfb3e5f5243e02c0321d10529bf
badreaz <baz.23.2019@gmail.com>
2023-02-16 08:46:53 -0800

    correct 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" 
-- Dora Korpar, 2015-10-19", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar,", 47);
+	write(1, " 2015-10-19\n", 12);
 	return (1);
 }
