d0761f529d90f63b83b998473e14f6e20a18a3fa
badreaz <baz.23.2019@gmail.com>
2023-02-17 07:24:50 -0800

    correct

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,16 +14,17 @@ int main(void)
 
 	num = 48;
 	num2 = 48;
-	num3 = 48;
-	num4 = 48;
 	while (num <= '9')
 	{
 		while (num2 <= '9')
 		{
+			num3 = 48;
 			while (num3 <= '9')
 			{
+				num4 = 48;
 				while (num4 <= '9')
 				{
+					num4++;
 					putchar(num);
 					putchar(num2);
 					putchar(' ');
@@ -34,12 +35,10 @@ int main(void)
 						putchar(',');
 						putchar(' ');
 					}
-					num4++;
 				}
 				num3++;
 			}
 			num2++;
-			num3 = num2 + 1;
 		}
 		num++;
 		num2 = num + 1;
