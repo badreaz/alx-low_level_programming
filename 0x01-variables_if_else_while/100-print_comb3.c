f116adb833591d264ed921b0e5e52b872895e0cf
badreaz <baz.23.2019@gmail.com>
2023-02-17 01:22:55 -0800

    create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of 2 numbers without repeating using putchar.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ascii;
+	int ascii2;
+
+	ascii = 48;
+	while (ascii <= '9'
+	{
+		putchar(ascii);
+		ascii2 = ascii + 1;
+		while (ascii2 <= '9')
+		{
+			putchar(ascii2);
+			if (ascii <= '9' && ascii2 <= '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			ascii2++;
+		}
+		ascii++;
+	}
+	putchar('\n');
+	return (0);
+}
