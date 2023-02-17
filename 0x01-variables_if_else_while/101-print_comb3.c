e2f01983dd4aaf8c595f678ae00fa383f1bdf80f
badreaz <baz.23.2019@gmail.com>
2023-02-17 06:46:40 -0800

    101-print_comb3.c

---
+++ 0x01-variables_if_else_while/101-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - pritn a combination of three numbers without repition using putchar.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	int num2;
+	int num3;
+
+	num = 48;
+	while (num <= '9')
+	{
+		num2 = num + 1;
+		while (num1 <= '9')
+		{
+			num3 = num2 + 1;
+			while (num3 <= '9')
+			{
+				putchar(num);
+				putchar(num2);
+				putchar(num3);
+				if (num < '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				num3++;
+			}
+			num1++;
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
