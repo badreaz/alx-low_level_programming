b8ffaa9ee5505bae11bc76cdc2d93b933d234543
badreaz <baz.23.2019@gmail.com>
2023-02-17 06:59:06 -0800

    create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - pritn a combination of four numbers without repition using putchar.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	int num2;
+	int num3;
+	int num4;
+
+	num = 48;
+	while (num <= '9')
+	{
+		num2 = num + 1;
+		while (num2 <= '9')
+		{
+			num3 = num2 + 1;
+			while (num3 <= '9')
+			{
+				num4 = num3 + 1;
+				while (num4 <= '9')
+				{
+					putchar(num);
+					putchar(num2);
+					putchar(' ');
+					putchar(num3);
+					if (num < '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					num4++;
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
