6021e5632b422847228d513a40b6fe8b070f6221
badreaz <baz.23.2019@gmail.com>
2023-02-21 00:17:16 -0800

    create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+
+/**
+ * jack_bauer - print every minute of the day.
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int digit;
+
+	i = 0;
+	while(i < 24)
+	{
+		j = 0;
+		while(j < 6)
+		{
+			k = 0;
+			while(k < 10)
+			{
+				digit = i % 100;
+				_putchar(digit + '0');
+				digit = i % 10;
+				_putchar(digit + '0');
+				_putchar(':');
+				_putchar(j + '0');
+				_putchar(k +'0');
+				_putchar('\n');
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
