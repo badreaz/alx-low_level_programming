67aee1e5677fae1703411db61b08866ca3a11282
badreaz <baz.23.2019@gmail.com>
2023-02-21 07:14:16 -0800

    correct

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void printnum(int, int);
 /**
  * print_times_table - print the n times table.
  * @n: number of times table.
@@ -15,36 +16,73 @@ void print_times_table(int n)
 		{
 			for (j = 0; j < n + 1; j++)
 			{
-				result = i * j;
-				if (j == 0)
-				{
-					_putchar('0');
-				}
-				else if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(((result / 10)  % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else if (result < 1000)
-				{
-					_putchar(' ');
-					_putchar(((result / 100) % 10) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				if (j < n)
-					_putchar(',');
+				printnum(i, j);
+				/*
+				* result = i * j;
+				* if (j == 0)
+				*{
+				*	_putchar('0');
+				*}
+				*else if (result < 10)
+				*{
+				*	_putchar(' ');
+				*	_putchar(' ');
+				*	_putchar(' ');
+				*	_putchar(result + '0');
+				*}
+				*else if (result < 100)
+				*{
+				*	_putchar(' ');
+				*	_putchar(' ');
+				*	_putchar(((result / 10)  % 10) + '0');
+				*	_putchar((result % 10) + '0');
+				*}
+				*else if (result < 1000)
+				*{
+				*	_putchar(' ');
+				*	_putchar(((result / 100) % 10) + '0');
+				*	_putchar(((result / 10) % 10) + '0');
+				*	_putchar((result % 10) + '0');
+				*}
+				*if (j < n)
+				*	_putchar(',');
+				*/
 			}
 			_putchar('\n');
 		}
 	}
+}
+/**
+ * printnum - print times teable template
+ * @i: i
+ * @j: j
+ */
+void printnum(int i, int j)
+{
+	result = i * j;
+	if (j == 0)
+		_putchar('0');
+	else if (result < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((result % 10) + '0');
+	}
+	else if (result < 100)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(((result / 10) % 10) + '0');
+		_putchar((result % 10) + '0');
+	}
+	else if (result < 1000)
+	{
+		_putchar(' ');
+		_putchar(((result / 100) % 10) + '0');
+		_putchar(((result / 10) % 10) + '0');
+		_putchar((result % 10) + '0');
+	}
+	if (j < n)
+		_putchar(',');
 }
