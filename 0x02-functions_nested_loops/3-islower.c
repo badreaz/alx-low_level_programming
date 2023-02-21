3fe02cf818411ce17e1194f19e1aacf1707b413c
badreaz <baz.23.2019@gmail.com>
2023-02-20 22:09:06 -0800

    create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+
+/**
+ * _islower - check if c is a lowercase.
+ *
+ * Return: 1 if c is lowercase otherwise 0.
+ */
+int _islower(int c)
+{
+	if (96 < c < 123)
+	{
+		return(1);
+	}
+	return(0);
+}
