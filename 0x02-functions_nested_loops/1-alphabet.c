0ac45eea73810722b04e5680d7a9ad095cd4d503
badreaz <baz.23.2019@gmail.com>
2023-02-20 21:53:32 -0800

    create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+
+/**
+ * print_alphabet - print lowercase alphabet using _putchar.
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while(alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	return(0);;
+}
