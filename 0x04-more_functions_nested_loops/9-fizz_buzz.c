22901b30f616981666356795f3bd3ce45a44d0e5
badreaz <baz.23.2019@gmail.com>
2023-02-23 03:17:43 -0800

    create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 100.
+ * Description: replace multiple by 3 or 5 to 
+ * Fizz Buzz respectively and FizzBuzz if both.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
