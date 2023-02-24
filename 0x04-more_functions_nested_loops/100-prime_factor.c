dcb7a86b34650317dd0f3110ba249945825329f4
badreaz <baz.23.2019@gmail.com>
2023-02-24 03:01:32 -0800

    create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest prime factor.
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int largest, i;
+	long int num = 612852475143;
+
+	for (i = 2; i <= num, i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+			largest = i;
+		}
+	}
+
+	printf("%d", largest);
+
+	return (0);
+}
