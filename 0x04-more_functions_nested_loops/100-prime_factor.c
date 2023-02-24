c32840260ba2d2447f7a80e766d7dd7d1d47c8a3
badreaz <baz.23.2019@gmail.com>
2023-02-24 03:06:46 -0800

    edit 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,19 +7,20 @@
  */
 int main(void)
 {
-	int largest, i;
+	int largest;
+	int i;
 	long int num = 612852475143;
 
-	for (i = 2; i <= num, i++)
+	for (i = 2; i <= num; i++)
 	{
 		if (num % i == 0)
 		{
-		num = num / i;
+			num = num / i;
 			largest = i;
 		}
 	}
 
-	printf("%d", largest);
+	printf("%d\n", largest);
 
 	return (0);
 }
