f610e32ce370c79b6c345d7a5f50860468f91713
badreaz <baz.23.2019@gmail.com>
2023-02-27 09:12:24 -0800

    create 101-kegen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random passwords.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, num;
+	char passsword[10];
+
+	srand((unsigned int)time(NULL));
+	
+	for (i = 1; i < 10; i++)
+	{
+		num = rand() % 128;
+		if (num > 47 && num < 58)
+		{
+			password[i] = num + '0';
+		}
+		else
+			i--;
+	}
+	password[10] = '\0';
+
+	return (0);
+}
