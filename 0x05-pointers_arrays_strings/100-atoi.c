3db10f29571ef1b560762a2b85001182ac232cfc
badreaz <baz.23.2019@gmail.com>
2023-02-27 06:42:42 -0800

    create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: pointer to a string.
+ *
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	int num = 0, n = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			n++;
+		else if (*s > 47 && *s < 58)
+		{
+			num = (*s - 48) * 10;
+			if (n % 2 != 0)
+				num *= -1;
+			n = 0;
+		}
+	}
+	return (num);
+}
