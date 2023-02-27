5affd96e0572fb7aa6fa477164720138123016b3
badreaz <baz.23.2019@gmail.com>
2023-02-26 21:52:39 -0800

    create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string.
+ * @str: pointer to string.
+ */
+void _puts(char *str)
+{
+	char *new;
+
+	while (*new != '\0' && p != NULL)
+	{
+		_putchar(new);
+		new++;
+	}
+	_putchar('\n');
+}
