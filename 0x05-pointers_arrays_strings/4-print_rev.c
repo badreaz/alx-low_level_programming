a93a51186246b270e053b2dabdae394fe1e37203
badreaz <baz.23.2019@gmail.com>
2023-02-26 22:03:09 -0800

    create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a srting in reverse.
+ * @s: pointer to char.
+ */
+void print_rev(char *s)
+{
+	int count = 0, n = 0;
+
+	while (*s != '\0' && s!= NULL)
+	{
+		count++;
+		s++;
+	}
+	while (n <= count)
+	{
+		s--;
+		_putchar(*s);
+		n++;
+	}
+	_putchar('\n');
+}
