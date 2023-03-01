ccea4d7ea2e36265bc59e11502d69d1fd3eb489d
badreaz <baz.23.2019@gmail.com>
2023-03-01 07:19:32 -0800

    edit 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int sum1 = 0, sum2 = 0;
 
-	while (*s1 != '\0')
+	while (s1 != 0)
 	{
 		sum1 += *s1;
 		s1++;
 	}
-	while (*s2 != '\0')
+	while (s2 != 0)
 	{
 		sum2 += *s2;
 		s2++;
