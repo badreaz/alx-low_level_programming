4f43d132ad08fc20bf979430f09373b5c4f3ae8a
badreaz <baz.23.2019@gmail.com>
2023-03-01 02:46:52 -0800

    create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings.
+ * @s1: string 1.
+ * @s2: string 2.
+ *
+ * Return: int.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int sum1 = 0, sum2 = 0;
+
+	while (*s1 != '\0')
+	{
+		sum1 += *s1;
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		sum2 += *s2;
+		s2++;
+	}
+	return (sum1 - sum2);
+}
