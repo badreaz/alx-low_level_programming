9ef641c7ad31296475c7e9f79d03d138d42b970c
badreaz <baz.23.2019@gmail.com>
2023-03-02 01:05:01 -0800

    create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar.
+ * @n: integer.
+ */
+void print_number(int n)
+{
+	int i = n, div = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	while (i / 10)
+	{
+		div += 10;
+		i /= 10;
+	}
+
+	while (div != 1)
+	{
+		i = n / div;
+		_putchar((i % 10) + '0');
+		div -= 10;
+	}
+}
