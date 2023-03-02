13a10f45075b3022f38b3c2a64c60221a20b3df1
badreaz <baz.23.2019@gmail.com>
2023-03-02 07:12:30 -0800

    edit 103-infintite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,35 +13,35 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, res, rem = 0;
 
-	for (i = 0; *(n1 + i); i++)
-	for (j = 0; *(n2 + j); j++)
-	if (size_r <= i + 1 || size_r <= j + 1)
+	for (i = 0; *(n1 + i) != '\0'; i++)
+	for (j = 0; *(n2 + j) != '\0'; j++)
+	if (size_r <= i + 2 || size_r <= j + 2)
 		return (0);
 	n1 += i - 1;
 	n2 += j - 1;
 	*(r + size_r) = '\0';
-	i--, j--, size_r--;
-	for (; n1[i] && n2[j]; i--, j--, size_r--)
+	size_r--;
+	for (; *n1 && *n2; n1--, n2--, size_r--)
 	{
-		res = (n1[i] - '\0') + (n2[j] - '\0') + rem;
-		r[size_r] = (res % 10) + '\0';
+		res = (*n1 - '0') + (*n2 - '0') + rem;
+		r[size_r] = (res % 10) + '0';
 		rem = res / 10;
 	}
-	for (; n1[i]; i--, size_r--)
+	for (; *n1; n1--, size_r--)
 	{
-		res = (n1[i] - '\0') + rem;
-		r[size_r] = (res % 10) + '\0';
+		res = (*n1 - '0') + rem;
+		r[size_r] = (res % 10) + '0';
 		rem = res / 10;
 	}
-	for (; n2[j]; j--, size_r--)
+	for (; *n2; n2--, size_r--)
 	{
-		res = (n2[j] - '\0') + rem;
-		r[size_r] = (n2[j] % 10) + '\0';
+		res = (*n1 - '0') + rem;
+		r[size_r] = (res % 10) + '0';
 		rem = res / 10;
 	}
 	if (rem && size_r >= 0)
 	{
-		r[size_r] = (rem % 10) + '\0';
+		r[size_r] = (rem % 10) + '0';
 		return (r + size_r);
 	}
 	else if (rem && size_r < 0)
