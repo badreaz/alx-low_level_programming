9aa6c5f35e3b3ba6c67151ac7dfbb969259fe8d9
badreaz <baz.23.2019@gmail.com>
2023-03-06 08:29:59 -0800

    edit 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,43 +5,25 @@
  * @haystack: pointer to string.
  * @needle: pointer to substring.
  *
- * Reurn: pointer to the beginning of located
+ * Return: pointer to the beginning of located
  * substring, or NULL.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char c;
-	char *new = needle;
-	int i, num, str, first;
+	char *n, *c;
 
-	i = 0;
-	while (haystack[i] != '\0')
+	while (*haystack != '\0')
 	{
-		c = haystack[i];
-		if (c != *needle)
+		c = haystack;
+		n = needle
+		while (*haystack == *n && *n != '\0')
 		{
-			i++;
-			str = 0;
-			needle = new;
-			first = 0;
+			haystack++;
+			n++;
 		}
-		else if (first == 0)
-		{
-			num = i;
-			first = 1;
-			i++;
-			str = 1;
-			needle++;
-		}
-		else if (str == 1)
-		{
-
-			i++;
-			needle++;
-		}
+		if (*n == '\0')
+			return (c);
+		haystack++;
 	}
-	if (str == 0)
-		return (0);
-
-	return (haystack + num);
+	return (0);
 }
