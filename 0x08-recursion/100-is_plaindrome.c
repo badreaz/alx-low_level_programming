460b39a7124480b716d4bcac2c94483589a532ce
badreaz <baz.23.2019@gmail.com>
2023-03-08 09:50:05 -0800

    add 100-is_plaindrome.c

---
+++ 0x08-recursion/100-is_plaindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_plaindrome - check if a string is plaindrome.
+ * @s: pointer to string.
+ *
+ * Return: 1 if true, 0 if false.
+ */
+int is_palindrome(char *s)
+{
+	if (*s== '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
