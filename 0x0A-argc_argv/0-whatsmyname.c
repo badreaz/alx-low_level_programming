aa1fbc7584a1e465795d0fe750edfd08c50e62ae
badreaz <baz.23.2019@gmail.com>
2023-03-10 04:40:58 -0800

    add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+
+/**
+ * main - print the executablle file name.
+ * @argc: words count.
+ * @argv: array of the input.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
