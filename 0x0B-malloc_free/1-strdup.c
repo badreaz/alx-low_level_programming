f7bc470b6c92bc7bd2bc4251205a13500d4734f3
badreaz <baz.23.2019@gmail.com>
2023-03-14 06:30:27 -0700

    edit 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,6 +16,7 @@ char *_strdup(char *str)
 		return (0);
 	while (str[size] != '\0')
 		size++;
+	size++;
 	p = malloc(sizeof(char) * size);
 	size = 0;
 	while (str[size] != '\0')
@@ -23,5 +24,6 @@ char *_strdup(char *str)
 		p[size] = str[size];
 		size++;
 	}
+	p[size] = '\0';
 	return (p);
 }
