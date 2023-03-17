6c601d7526c59313bc700db27756d8324fcc0e0f
badreaz <baz.23.2019@gmail.com>
2023-03-17 09:09:08 -0700

    edit 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
+/**
+ * not_number - check if their letter in number.
+ * @array: pointer to charcater.
+ *
+ * Return: 0 (sucess) otherwise 1.
+ */
+int not_number(char *argv[])
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < strlen(argv[1]); i++)
+	{
+		if (!(argv[1][i] > 47 && argv[1][i] < 58))
+			return (1);
+	}
+	for (i = 0; i < strlen(argv[2]); i++)
+	{
+		if (!(argv[2][i] > 47 && argv[2][i] < 58))
+			return (1);
+	}
+	return (0);
+}
 /**
  * main - multiplies two positive numbers.
  * @argc: number of arguments.
@@ -12,11 +35,12 @@ int main(int argc, char *argv[])
 {
 	int mul, num1, num2;
 
-	if (argc != 3)
+	if (argc != 3 || not_number(argv))
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	mul = num1 * num2;
