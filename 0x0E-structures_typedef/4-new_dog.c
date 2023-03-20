796804087432af08019a87bef3a73be692160e81
badreaz <baz.23.2019@gmail.com>
2023-03-20 08:27:49 -0700

    edit 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,30 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *cpname = name;
-	char *cpowner = owner;
+	char *mname;
+	char *mowner;
+	int i, j, n;
 
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	for (i = 0; name[i]; i++)
+		;
+	mname = malloc(sizeof(char) * i);
+	for (j = 0; owner[j]; j++)
+		;
+	mowner = malloc(sizeof(char) * j);
+	if (dog == NULL || mname == NULL || mowner == NULL)
+	{
+		free(mname);
+		free(mowner);
+		free(dog);
 		return (NULL);
-	dog->name = cpname;
+	}
+	for (n = 0; n <= i; n++)
+		mname[n] = name[n];
+	for (n = 0; n <= j; n++)
+		mowner[n] = owner[n];
+	dog->name = mname;
 	dog->age = age;
-	dog->owner = cpowner;
+	dog->owner = mowner;
 	return (dog);
 }
