dfcef65c7a6b5eca1d160e7000ab31e8363a39d3
badreaz <baz.23.2019@gmail.com>
2023-03-30 09:51:28 -0700

    edit 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,10 +6,11 @@
  */
 void free_list(list_t *head)
 {
-	if (head->next)
-		free_list(head->next);
-	if (head->str)
-		free(head->str);
 	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
 		free(head);
+	}
 }
