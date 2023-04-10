d0d482af0d81718aa27e051abda7ef9389c44ad3
badreaz <baz.23.2019@gmail.com>
2023-04-10 12:21:31 -0700

    fix betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,16 +16,12 @@ int main(int argc, char *argv[])
 	mode_t mod = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mod);
 	i = 1;
 	while (i)
@@ -44,14 +40,10 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (close(from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", from), exit(100);
+
 	if (close(to) == -1)
-        {
-                dprintf(2, "Error: Can't close fd %d\n", to);
-                exit(100);
-        }
+		dprintf(2, "Error: Can't close fd %d\n", to), exit(100);
+
 	return (0);
 }
