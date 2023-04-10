507a769191c135f9c5bf23fab81055daeb345534
badreaz <baz.23.2019@gmail.com>
2023-04-10 12:44:55 -0700

    fix while loop

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,29 +11,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t count, i, end;
-	FILE *textfile;
+	size_t count;
+	int textfile, let, i;
 	char text[1024];
 
 	if (filename == NULL || letters == 0)
 		return (0);
-	textfile = fopen(filename, "r");
-	if (textfile == NULL)
+	textfile = open(filename, O_RDONLY);
+	if (textfile == -1)
 		return (0);
 
 	count = 0;
-	end = 1;
-	while (count < letters && end)
+	let = 1;
+	while (count < letters && let != 0)
 	{
-		for (i = 0; i < 1024; i++)
-			text[i] = '\0';
-		if (!fgets(text, 1024, textfile))
-			end = 0;
-		for (i = 0; text[i] && count < letters; i++, count++)
-			write(1, &text[i], 1);
+		let = read(textfile, text, letters - count);
+		count += let;
+		i = write(1, text, let);
+		if (let == -1 || i == -1 || i != let)
+		{
+			close(textfile);
+			return (0);
+		}
 	}
-	if (count < letters)
-		return (0);
-	fclose(textfile);
+	close(textfile);
 	return (count);
 }
