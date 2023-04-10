3a75f1e108808483a364b6d2521bb5ea17730514
badreaz <baz.23.2019@gmail.com>
2023-04-10 05:27:48 -0700

    add failing return

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
+	if (sizeof(n) / 4 < (index * index))
+		return (-1);
 	bit = (n >> index) & 1;
 	return (bit);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,6 +10,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (sizeof(*n) / 4 < (index * index))
+		return (-1);
 	*n = *n | (1 << index);
 	return (1);
 }
