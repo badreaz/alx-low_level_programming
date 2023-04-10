b0fa9bc2e9a0b666b90c3ebf0b0b58d087251f99
badreaz <baz.23.2019@gmail.com>
2023-04-10 12:25:02 -0700

    close file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		if (write(file, text_content, size) == -1)
+		{
+			close(file);
 			return (-1);
+		}
 	}
 	close(file);
 	return (1);
