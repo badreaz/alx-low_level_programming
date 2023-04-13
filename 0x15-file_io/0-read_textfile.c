29bf0be0ed337303042a83e7d646ff28fc2d0cc3
badreaz <baz.23.2019@gmail.com>
2023-04-13 03:48:17 -0700

    modify posix stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	let = read(textfile, text, letters);
-	i = write(1, text, let);
+	i = write(STDOUT_FILENO, text, let);
 
 	free(text);
 	close(textfile);
