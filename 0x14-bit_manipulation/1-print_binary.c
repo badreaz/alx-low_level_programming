6f8d50457e034ab01010569070969b0a63d456a6
badreaz <123468928+badreaz@users.noreply.github.com>
2023-05-04 14:46:18 +0400

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * get_endianness - checks the endianness.
- *
- * Return: 0 if big endian, 1 if little endian.
- */
-
-int get_endianness(void)
-{
-        unsigned int n = 6;
-        char *ptr = (char *)&n;
-
-        if (*ptr)
-                return (1);
-        return (0);
-}
-Hol
-#include "main.h"
-
-/**
  * print_binary - prints the binary representation of a number.
  * @n: nummber.
  */
