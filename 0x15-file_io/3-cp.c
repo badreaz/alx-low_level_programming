d25e03564802ff5cc18c014f462b76eea7ac9d09
badreaz <baz.23.2019@gmail.com>
2023-05-08 18:52:10 +0400

    add POSIX stderr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	long int from, to;
 	char buffer[1024];
 	long int i, j;
-	mode_t mod = 00664; /*S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;*/
+	mode_t mod = 0664; /*S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;*/
 
 	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
@@ -29,21 +29,21 @@ int main(int argc, char *argv[])
 		i = read(from, buffer, 1024);
 		if (i == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		j = write(to, buffer, i);
 		if (j == -1 || j != i)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (close(from) == -1)
-		dprintf(2, "Error: Can't close fd %ld\n", from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", from), exit(100);
 
 	if (close(to) == -1)
-		dprintf(2, "Error: Can't close fd %ld\n", to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", to), exit(100);
 
 	return (0);
 }
