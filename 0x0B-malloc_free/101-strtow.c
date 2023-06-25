4810276d3123ce52deeeaad4ed2c9bd943a940fd
badreaz <baz.23.2019@gmail.com>
2023-06-25 16:11:27 +0400

    delete project 14 & 15

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * strtow - splits a string into words.
@@ -11,27 +12,28 @@
  */
 char **strtow(char *str)
 {
-	char **ret;
-	int count[50];
-	int i = 0, j = 0, n = 0, c = 1;
+	char **ret, *ptr;
+	int i, j, flag = 1;
+	/*int count[50];
+	int i = 0, j = 0, n = 0, c = 1;*/
 
 	if (str == NULL || *str == 0)
 		return (NULL);
-	while (str[i] != '\0')
+	/*while (str[i] != '\0')
 	{
 		for (; str[i] == 32; i++)
-		for (; str[i] != 32 && str[j]; i++)
+		for (; str[i] != 32 && str[i]; i++)
 			c++;
 		count[n] = c;
 		n++;
-	}
-	ret = malloc(sizeof(void *) * n);
+	}*/
+	/*ret = malloc(sizeof(char *) * n);
 	if (ret == NULL)
 	{
 		free(ret);
 		return (NULL);
-	}
-	c = 0;
+	}*/
+	/*c = 0;
 	while (str[j] != '\0')
 	{
 		ret[c] = malloc(sizeof(char) * count[c]);
@@ -42,13 +44,82 @@ char **strtow(char *str)
 			free(ret);
 			return (NULL);
 		}
-		for (; str[j] == 32; j++)
-			;
+		for (; str[j]; j++)
+		{
+			if (str[j] != 32)
+				break;
+		}
+		for (i = 0; str[j] != 32 && str[j]; j++, i++)
+			ret[c][i] = str[j];
+		ret[c][i] = '\0';
+		c++;
+	}*/
+	/*while (str[j] != '\0')
+	{
+		if (str[j] == 32)
+		{
+			for (; str[j] == 32; j++)
+				;
+		}
+		ret[c] = malloc(sizeof(char) * count[c]);
+		if (ret[c] == NULL)
+		{
+			for (i = 0; i <= c; i++)
+				free(ret[i]);
+			free(ret);
+			return (NULL);
+		}
 		for (i = 0; str[j] != 32 && str[j]; j++, i++)
 			ret[c][i] = str[j];
 		ret[c][i] = '\0';
 		c++;
 	}
-	ret[c] = NULL;
+	ret[c] = NULL;*/
+	for (i = j = 0; str[i]; i++)
+	{
+		if (str[i] == 32)
+		{
+			printf("hi");
+			flag = 1;
+		}
+		else if (flag)
+		{
+			printf("hello");
+			flag = 0;
+			j++;
+		}
+	}
+	ret = malloc(sizeof(char *) * j);
+	if (ret == NULL)
+	{
+		free(ret);
+		return (NULL);
+	}
+	for (i = j = 0; str[i]; i++)
+	{
+		if (str[i] == 32)
+		{
+			str[i] = '\0';
+			flag = 1;
+		}
+		else if (flag)
+		{
+			ret[j] = &str[i];
+			flag = 0;
+			j++;
+		}
+	}
+
+	for (i = 0; str; i++)
+	{
+		ptr = str;
+		ptr = _strpbrk(ptr, " ");
+		if (ptr != NULL)
+		{
+			ptr = '\0';
+			ret[i] = ptr + 1;
+		}
+		str++;
+	}
 	return (ret);
 }
