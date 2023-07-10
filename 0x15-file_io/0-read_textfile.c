81b1d54465f573a352297aa7b6a635ad091ff12d
badreaz <baz.23.2019@gmail.com>
2023-07-10 15:56:02 +0400

    create task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to
+ *  the POSIX standard output.
+ * @filename: file to read.
+ * @letters: number of characters to read.
+ *
+ * Return: number of characters printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t textfile, let, i;
+	char *text;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	textfile = open(filename, O_RDONLY);
+	if (textfile == -1)
+		return (0);
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
+		return (0);
+
+	let = read(textfile, text, letters);
+	i = write(STDOUT_FILENO, text, let);
+
+	free(text);
+	close(textfile);
+	return (i);
+}
