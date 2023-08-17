3988d92f132557e62674920b32c4309478c02775
badreaz <baz.23.2019@gmail.com>
2023-08-17 22:26:36 +0400

    handle list end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx)
 		{
+			if (!ptr->next)
+				return (add_dnodeint_end(h, n));
 			new = malloc(sizeof(dlistint_t));
 			if (new == NULL)
 				return (NULL);
