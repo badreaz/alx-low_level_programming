f21a18f06449b73f6402a4ccc1d494e5a6374124
badreaz <baz.23.2019@gmail.com>
2023-08-17 19:02:15 +0400

    handle failure

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,18 +5,18 @@
  * @head: pointer to the head of list.
  * @n: int to add.
  *
- * REturn: the address of the new element, or NULL if it failed.
+ * Return: the address of the new element, or NULL if it failed.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *ptr = *head, *new;
+	dlistint_t *new;
 
-	while (ptr->next)
-		ptr = ptr->next;
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL):
 	new->n = n;
-	new->prev = ptr;
-	new->next = NULL;
-	ptr->next = new;
+	new->prev = NULL;
+	new->next = *head;
+	*head->prev = new;
 	return (new);
 }
