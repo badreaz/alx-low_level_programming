acc8a6515a59eb023a68f801373e4cd5fe4054cf
badreaz <baz.23.2019@gmail.com>
2023-08-21 21:52:53 +0400

    update

--- 0x18-dynamic_libraries/rand.c
+++ 0x18-dynamic_libraries/rand.c
@@ -4,15 +4,15 @@ int rand(void)
 
 	n++;
 	if (n == 0)
-		return (9);
+		return (8);
 	else if (n == 1)
 		return (8);
 	else if (n == 2)
-		return (10);
+		return (7);
 	else if (n == 3)
-		return (24);
-	else if (n == 4)
-		return (75);
-	else if (n == 5)
 		return (9);
+	else if (n == 4)
+		return (23);
+	else if (n == 5)
+		return (74);
 }
