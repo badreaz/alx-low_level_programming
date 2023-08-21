f3ad544d0610d7217eac74d63249b3379b97e425
badreaz <baz.23.2019@gmail.com>
2023-08-21 22:23:46 +0400

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/badreaz/alx-low_level_programming/raw/master/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=/tmp/rand.so
+wget -P /tmp https://github.com/badreaz/alx-low_level_programming/raw/master/0x18-dynamic_libraries/print.so
+export LD_PRELOAD=/tmp/print.so
