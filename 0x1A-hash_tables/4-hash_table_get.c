33d0c661e765d2d067ef24687bbe16e72ed54a67
badreaz <baz.23.2019@gmail.com>
2023-09-07 18:57:02 +0400

    create 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table.
+ * @key: key.
+ *
+ * Return: value associated with key, or NULL if couldn't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	if (ht == NULL)
+		return (NULL);
+	index = key_index(key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(key, ht->array[i]->key) == 0)
+			return (ht->array[i]->value);
+	}
+	return (NULL);
+}
