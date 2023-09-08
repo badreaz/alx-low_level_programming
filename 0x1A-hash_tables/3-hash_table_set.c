8b78da3cbc2bd8e2c49c2be3e4333c927b3dbe9d
badreaz <baz.23.2019@gmail.com>
2023-09-08 15:09:13 +0400

    check if key exists

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index, i;
 
-	if (ht == NULL || value == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
