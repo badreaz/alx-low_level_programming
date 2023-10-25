0ae791f1a8709c8cddc6ac9699dfa55ed75d184b
badreaz <baz.23.2019@gmail.com>
2023-10-25 20:41:03 +0400

    create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" island_perimeter function module """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island
+    described in rid """
+    num = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            try:
+                if grid[i][j] != grid[i][j + 1]:
+                    num += 1
+                if grid[i][j] != grid[i + 1][j]:
+                    num += 1
+            except IndexError:
+                continue
+    return num
+
+if __name__ == "__main__":
+    grid = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+            ]
+    print(island_perimeter(grid))
