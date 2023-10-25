ef9064344f8166b11635b563f9b5b0041fa3acb4
badreaz <baz.23.2019@gmail.com>
2023-10-25 21:08:59 +0400

    correct conditions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,11 +12,11 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             try:
-                if (j == 0 or j + 1 == len(grid[i]) - 1) and grid[i][j]:
+                if (j == 0 or j == len(grid[i]) - 1) and grid[i][j]:
                     num += 1
                 if grid[i][j] != grid[i][j + 1]:
                     num += 1
-                if (i == 0 or i + 1 == len(grid) - 1) and grid[i][j]:
+                if (i == 0 or i == len(grid) - 1) and grid[i][j]:
                     num += 1
                 elif grid[i][j] != grid[i + 1][j]:
                     num += 1
